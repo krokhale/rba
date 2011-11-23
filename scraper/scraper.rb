68c4e68eedfbcfafa08570eae03e444eb5b4d316
krishna rokhale <krishna.rokhale@gmail.com>
2011-11-23 12:25:04 -0800

    added open uri.

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -2,19 +2,21 @@ module TimeSeries
   class Scraper
 
     def initialize
-       @url = "www.rba.gov.au/statistics/tables"
+       @url ||= "www.rba.gov.au/statistics/tables"
     end
 
     def run!
-      get_xls_files
+      parse_xls_links
       parse_xls_files
     end
 
     private
 
-    def get_xls_files
-
-
+    def parse_xls_links
+      spans, links = [], []
+      doc = Nokogiri::HTML(open(@url))
+      doc.search("//span").map {|span| spans << span if span.text == "[XLS]"}
+      #spans.map {|span| }
     end
 
     def parse_xls_files
